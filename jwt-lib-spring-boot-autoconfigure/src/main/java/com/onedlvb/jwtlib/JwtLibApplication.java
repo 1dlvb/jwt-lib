c861e461a5c427d41e646cfb725fcd615f3f0219
1dlvb <matushkin-2017@mail.ru>
2024-07-30 22:59:38 +0500

    chore: initial commit

---
+++ jwt-lib-spring-boot-autoconfigure/src/main/java/com/onedlvb/jwtlib/JwtLibApplication.java
@@ -0,0 +1,13 @@
+package com.onedlvb.jwtlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLibApplication.class, args);
+    }
+
+}
---
+++ jwt-lib-spring-boot-autoconfigure/src/main/java/com/onedlvb/jwtlib/util/Roles.java
@@ -0,0 +1,18 @@
+package com.onedlvb.jwtlib.util;
+
+/**
+ * Enum for handling roles.
+ * @author Matushkin Anton
+ */
+public enum Roles {
+
+    USER,
+    CREDIT_USER,
+    OVERDRAFT_USER,
+    DEAL_SUPERUSER,
+    CONTRACTOR_RUS,
+    CONTRACTOR_SUPERUSER,
+    SUPERUSER,
+    ADMIN
+
+}
